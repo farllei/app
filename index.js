ef81774eb5ee0b53fe6e3d20e5dba94d27178433
Farllei Arcanjo <farllei.harrison@icloud.com>
2024-09-10 16:30:31 +0100

    fundamentos 003

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
 // Intro
 
 // Let variable can have its value changed later
-let mensagem = "Hello world!"
+// let mensagem = "Hello world!"
 
 // Const variable cannot have its value changed later 
-const mensagem1 = "Hello World1"
+// const mensagem1 = "Hello World1"
 
 //--- Example or inner scope
 // variables inside inner scopes are read first
-{
-    const mensagem = "Olá mundo!"
-    console.log(mensagem);
-}
+//{
+//    const mensagem = "Olá mundo!"
+//    console.log(mensagem);
+//}
 
-console.log(mensagem);
+// console.log(mensagem);
 
 // arrays, objects
 
-const number = [1,2,3,4,5]
-let meta = {
-    value: "Ler um livro por mês",
-    address: "2, Palm street",
-    checked: false,
-    log: (info)=> {
-        console.log(info);
-    }
-}
-meta.value = "Não é mais ler um livro";
-meta.log(meta.value);
+// const number = [1,2,3,4,5]
 
 // function // arrow function
-const criarMeta = () => {}
+// const criarMeta = () => {}
 
 // named function
 //function criarMeta(){}
+
+let meta = {
+    value: "Ler um livro por mês",
+    checked: true,
+}
+
+let metas = [
+    meta,
+    {
+        value: "Caminhar 20 min todos os dias",
+        checked: false 
+    }
+]
+
+console.log(metas[1].value);
