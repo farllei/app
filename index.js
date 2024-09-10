bb1e358cf3e561819bb4547f8e62fd7c1f4873db
Farllei Arcanjo <farllei.harrison@icloud.com>
2024-09-10 16:16:12 +0100

    fundamentos 002

--- index.js
+++ index.js
@@ -9,8 +9,28 @@ const mensagem1 = "Hello World1"
 //--- Example or inner scope
 // variables inside inner scopes are read first
 {
-    const mensage = "Olá mundo!"
+    const mensagem = "Olá mundo!"
     console.log(mensagem);
 }
 
 console.log(mensagem);
+
+// arrays, objects
+
+const number = [1,2,3,4,5]
+let meta = {
+    value: "Ler um livro por mês",
+    address: "2, Palm street",
+    checked: false,
+    log: (info)=> {
+        console.log(info);
+    }
+}
+meta.value = "Não é mais ler um livro";
+meta.log(meta.value);
+
+// function // arrow function
+const criarMeta = () => {}
+
+// named function
+//function criarMeta(){}
